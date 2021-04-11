e12ba3404f3d67ad65e011808c330636c1831ef5
bricha-fz <bricha.fz@gmail.com>
2021-04-11 17:35:37 +0200

    update vaccination

--- covid-2021-app/src/pages/Vaccination.vue
+++ covid-2021-app/src/pages/Vaccination.vue
@@ -7,28 +7,14 @@
 
     
   <div class="flex justify-center">
-
-    <q-card-section>
-      <div class="flex justify-center">
-        <div class="row">
-          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-white " style="max-width: 300px; max-height:300px; background-color:#99c7cd; ">
-              <div class="text-h6 text-weight-bold">Nombre total de personnes vaccinées </div>
-              <div class="text-caption text-italic">Cumul de personnes ayant reçu une dose ou deux doses de vaccin<br><div class="text-h3 text-weight-bold"> {{cum_dose1}}</div></div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-    </q-card-section>
-
   
     <q-card-section>
       <div class="flex justify-center">
         <div class="row">
-          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-white " style="max-width: 300px; background-color: #99c7cd;">
+          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:400px; lenghth:400px;">
+            <q-card-section class="text-white " style="max-width: 400px; background-color: #32584E;">
               <div class="text-h6 text-weight-bold">Nombre de personnes ayant reçu la 1ere dose</div>
-              <div class="text-caption text-italic" >*Deux doses sont nécessaires pour considérer qu'une personne est vaccinée<br><div class="text-h3 text-weight-bold"> {{cum_dose1}}</div></div>
+              <div class="text-overline " >*Deux doses sont nécessaires pour considérer qu'une personne est vaccinée<br><div class="text-h3 text-weight-bold text-center"> {{cum_dose1}}</div></div>
             </q-card-section>
           </q-card>
         </div>
@@ -38,71 +24,108 @@
     <q-card-section>
       <div class="flex justify-center">
         <div class="row">
-          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-white " style="max-width: 300px; background-color: #99c7cd;">
-              <div class="text-h6 text-weight-bold">Nombre de personnes ayant reçues deux doses de vaccin<br><div class="text-h3 text-weight-bold"> {{cum_dose2}}</div></div>
-              <div class="text-caption text-italic"></div>
+          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:400px;lenghth:400px;">
+            <q-card-section class="text-white " style="max-width: 400px; background-color: #32584E;">
+              <div class="text-h6 text-weight-bold">Nombre de personnes <br>vaccinées</div>
+              <div class="text-overline ">*nombre total des personnes ayant reçu les deux doses de vaccin <br><div class="text-h3 text-weight-bold"> {{cum_dose2}}</div></div>
             </q-card-section>
           </q-card>
         </div>
       </div>
     </q-card-section>
   </div>
-
-  <div class="flex justify-center">
-
   
-    <q-card-section>
-      <div class="flex justify-center">
-        <div class="row">
-          <q-card class="my-card q-pt" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-teal " style="max-width: 300px;">
-              <div class="text-h6">Nombre total des personnes vaccinées</div>
-              <div class="text-subtitle2">subtitle just in case</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-    </q-card-section>
+  <div class= "q-pa-lg" ></div>
 
-    <q-card-section>
-      <div class="flex justify-center">
-        <div class="row">
-          <q-card class="my-card q-pt" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-teal " style="max-width: 300px; max-height:300px; ">
-              <div class="text-h6">Nombre de personnes ayant reçues une première dose</div>
-              <div class="text-subtitle2">subtitle just in case</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-    </q-card-section>
-  </div>
-
+<q-card class="my-card q-pa-xl ">
   <div class="flex justify-center">
-  <div class="q-pa-lg q-gutter-sm ">
-    <q-banner class="shadow-4 text-black" style="background-color: white; text-align:center; max-width:800px;max-height:100px">
-      <div class="text-h5 text-weight-bold" style="color: #99c7cd">
-        Rechercher les informations par département
-      </div>
-      
-      
-    </q-banner>
-    </div>
+          <q-card class="my-card q-pa-md my-card q-pt shadow-10 " style="margin-bottom:70px;text-align:center; width:850px;max-height:100px;">
+            <q-card-section class="text-black" >
+              <div style ="font-size:30px;color:#32584E;font-weight:bold;"> L'évolution de la vaccination en France</div>
+            </q-card-section>
+          </q-card>
+        </div>
+        </q-card>
+
+
+  <div class="q-ma-auto flex flex-center">
+    <q-circular-progress
+      show-value 
+
+      font-size="70px"
+      class="text-grey-8 text-weight-bold q-ma-md"
+      :value="percent_dose1"
+      :min= "0"
+      :max= "100"
+      size="350px"
+      :thickeness = "0.2"
+      color="teal-10"
+      center-color="white"
+      track-color="teal-2"
+    >{{percent_dose1}} %
+    <div class="my-card q-pt" style="margin:10px;text-align:center;width:300px;">
+            <div class="text-teal-10 " style="max-width: 300px;">
+              <div class="text-weight-bold"><br><br>Pourcentage des personnes ayant<br> reçu une première dose en France.</div>
+            </div>
+          </div></q-circular-progress>
+
+          <div class="q-ma-xl "></div>
+
+    <q-circular-progress
+      show-value 
+
+      font-size="70px"
+      class="text-grey-8 text-weight-bold q-ma-md"
+      :value="percent_dose2"
+      :min= "0"
+      :max= "100"
+      size="350px"
+      :thickeness = "0.2"
+      color="teal-10"
+      center-color="white"
+      track-color="teal-2"
+    >{{percent_dose2}} %
+    <div class="my-card q-pt" style="margin:10px;text-align:center;width:300px;">
+            <div class="text-teal-10 " style="max-width: 300px;">
+              <div class="text-weight-bold"><br><br>Pourcentage des personnes ayant<br> les deux doses en France.</div>
+            </div>
+          </div>
+    </q-circular-progress>
+    
     </div>
 
+
+      <div class= "q-pa-lg" ></div>
+
+<q-card class="my-card q-pa-xl ">
+  <div class="flex justify-center">
+          <q-card class="my-card q-pa-md my-card q-pt shadow-10 " style="margin-bottom:70px;text-align:center; width:850px;max-height:100px;">
+            <q-card-section class="text-black" >
+              <div style ="font-size:30px;color:#32584E;font-weight:bold;"> Rechercher les informations par département</div>
+            </q-card-section>
+          </q-card>
+        </div>
+        </q-card>
+
+
+        
+        <div class="flex justify-center">
     <div class="flex justify-center">
-    <div class="col" style="max-width:500px;">
+    <div class="col" style="width:500px;">
             <q-select color="grey-3" outlined style="color:#32584E;" v-model="model" :options="selectOptions" label="Sélectionner un département">
               <template v-slot:prepend>
                 <q-icon name="place"/>
               </template>
             </q-select>
     </div>
-    <div class="col"><q-btn style="padding:10px;margin-left:40px;width:200px; background-color:teal;font-size:14px;" text-color="white" @click ="getInfoVaxDep" label="Vaccination" /></div>
+    <div class="col"><q-btn style="padding:10px;margin-left:150px;width:200px; background-color:#3d7062;font-size:14px;" text-color="white" @click ="getInfoVaxDep" label="Rechercher" /></div>
         </div>
       <div style="text-align:center; color:#d64d4d;font-size:14px;font-weight:bold;"> {{ erreur }}</div>
     </div>
+    </div>
+
+
+   
 
     <div class="flex justify-center">
     
@@ -110,10 +133,10 @@
     <q-card-section>
       <div class="flex justify-center">
         <div class="row">
-          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-white " style="max-width: 300px; max-height:300px; background-color:#99c7cd; ">
-              <div class="text-h6 text-weight-bold">Nombre de personnes ayant reçu la 1ere dose </div>
-              <div class="text-caption text-italic">*Deux doses sont nécessaires pour considérer qu'une personne est vaccinée<br><div class="text-h3 text-weight-bold"> {{dose1_dep}} </div></div>
+          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:400px; lenghth:400px;">
+            <q-card-section class="text-white " style="max-width: 400px; background-color: #32584E;">
+              <div class="text-h6 text-weight-bold">Nombre de personnes ayant reçu la 1ere dose</div>
+              <div class="text-overline " >*Deux doses sont nécessaires pour considérer qu'une personne est vaccinée<br><div class="text-h3 text-weight-bold text-center"> {{dose1_dep}}</div></div>
             </q-card-section>
           </q-card>
         </div>
@@ -124,16 +147,20 @@
     <q-card-section>
       <div class="flex justify-center">
         <div class="row">
-          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:300px;">
-            <q-card-section class="text-white " style="max-width: 300px; background-color: #99c7cd;">
-              <div class="text-h6 text-weight-bold">Nombre de personnes ayant reçu les deux doses de vaccin</div>
-              <div class="text-caption text-italic" >Nombre de personnes ayant reçues deux doses de vaccin<br><div class="text-h3 text-weight-bold"> {{dose2_dep}}</div></div>
+          <q-card class="my-card q-pt shadow-10" style="margin:10px;text-align:center;width:400px;lenghth:400px;">
+            <q-card-section class="text-white " style="max-width: 400px; background-color: #32584E;">
+              <div class="text-h6 text-weight-bold">Nombre de personnes <br>vaccinées</div>
+              <div class="text-overline ">*nombre total des personnes ayant reçu les deux doses de vaccin <br><div class="text-h3 text-weight-bold"> {{dose2_dep}}</div></div>
             </q-card-section>
           </q-card>
         </div>
       </div>
     </q-card-section>
     </div>
+
+
+    
+
 
 
     <div class="q-pa-lg q-gutter-sm ">
@@ -176,6 +203,8 @@ name: "Vaccination",
     erreur:"",
     dose1_dep: "",
     dose2_dep: "",
+    percent_dose1: "",
+    percent_dose2: "",
   }
   },
 
@@ -192,6 +221,9 @@ async mounted() {
 
     }
     this.cum_dose1 = n_cum_dose1;
+    let round_dose1 = (n_cum_dose1/67064000)*100;
+    let percent_1 = round_dose1.toFixed(2);
+    this.percent_dose1 = percent_1;
 
 
     let n_cum_dose2 = 0;
@@ -201,6 +233,10 @@ async mounted() {
 
     }
     this.cum_dose2 = n_cum_dose2;
+    let round_dose2 = (n_cum_dose2/67064000)*100;
+    let percent_2 = round_dose2.toFixed(2);
+    this.percent_dose2 = percent_2;
+    
 
 
 
@@ -209,6 +245,9 @@ async mounted() {
 
       this.selectOptions.push(dep_name);
     }
+
+    
+
 
 
   },
@@ -230,6 +269,10 @@ async mounted() {
         
       }
     }
+
+    // async displayPercentage(){
+
+    // }
 
   }
 
